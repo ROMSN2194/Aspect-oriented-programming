e433a02e2139284abea0f813c2dc9aed080e571a
ROMSN2194 <lebedovichroman1@i.ua>
2017-02-02 07:01:02 +0200

    Time in seconds

--- MethodInvocationStatistic/MethodInvocationStatistic/MethodStatistics.cs
+++ MethodInvocationStatistic/MethodInvocationStatistic/MethodStatistics.cs
@@ -1,36 +1,39 @@
-﻿using System.Collections.Generic;
+using AspectInjector.Broker;
+using NLog;
+using System;
 using System.Diagnostics;
-using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace MethodInvocationStatistic
 {
-    internal class StackTraceUtils
+   internal class MethodStatistics
     {
+        private static readonly Logger log = LogManager.GetCurrentClassLogger();
+        private string methodName;
+        private Stopwatch timer;
         
-        private StackTrace stackTrace;
-        private IList<StackFrame> callers;
-        internal StackTraceUtils(StackTrace stackTrace)
+        [Advice(InjectionPoints.Before, InjectionTargets.Method)]
+        public void OnEntry()
         {
-            this.stackTrace = stackTrace;
-            callers = new List<StackFrame>(stackTrace.GetFrames().Reverse());
+            StackTrace stack = new StackTrace();
+            
+            StackTraceUtils stackUtils = new StackTraceUtils(stack);
+            
+            methodName = stackUtils.GetCurrentMethodName();
+            log.Info("Method " + methodName);
+
+            string callers =  stackUtils.GetStackTraceMethods();
+            timer = new Stopwatch();
+            timer.Start();
+            
+            log.Info("Call stack: {0}", callers);
         }
 
-        internal string GetStackTraceMethods()
+        [Advice(InjectionPoints.After, InjectionTargets.Method)]
+        public void OnExit()
         {
-            string callStackString = "";
-          
-            // To ignore .NET standard invocated methods like Run(), ExecuteAssembly()
-            for (int i = 8; i < callers.Count - 1; i++)
-            {
-                callStackString += callers[i].GetMethod() + "=>";
-            }
-
-            return callStackString;
-        }
-
-        internal string GetCurrentMethodName()
-        {
-            return callers[callers.Count - 2].GetMethod().ToString();
+            timer.Stop();
+            log.Info("Execution time: {0} seconds" , timer.ElapsedMilliseconds/1000);
         }
     }
 }
